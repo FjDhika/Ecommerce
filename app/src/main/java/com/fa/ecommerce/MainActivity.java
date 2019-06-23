0fc911564efeabd6158a7ed49a39d36cd911bd3b
MavisClouds <klogi99@gmail.com>
2019-06-23 19:35:41 +0700

    REGIS WOOIII

--- app/src/main/java/com/fa/ecommerce/MainActivity.java
+++ app/src/main/java/com/fa/ecommerce/MainActivity.java
@@ -3,10 +3,12 @@ package com.fa.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button LoginBtn,SignUpBtn;
 
@@ -16,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LoginBtn =  (Button) findViewById(R.id.signin_btn);
-        SignUpBtn = (Button) findViewById(R.id.signup_btn);
+        Button SignUpBtn = findViewById(R.id.signup_btn);
+        SignUpBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.signup_btn:
+                Intent movetoregis = new Intent(MainActivity.this, Register.class);
+                startActivity(movetoregis);
+        }
     }
 }
